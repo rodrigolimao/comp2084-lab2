2b7b347705288f4267a28a4a84828e527c5db2c0
Rodrigo Lima Camargo Lima <200380021@student.georgianc.on.ca>
2018-09-23 14:44:22 -0400

    Add project files.

---
+++ Lab2/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details (string ProductName)
+        {
+            ViewBag.ProductName = ProductName;
+            return View();
+
+        }
+    }
+}
